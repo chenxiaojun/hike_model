73b2d136935a72211840fc8670583b53fdee0723
Ricky <rickycxj@gmail.com>
2018-06-13 11:31:15 +0800

    用户发帖

---
+++ topic.rb
@@ -0,0 +1,29 @@
+class Topic < ApplicationRecord
+  include TopicCountable
+  include Excellentable
+
+  belongs_to :user
+  has_one    :topic_counter, dependent: :destroy
+  serialize :images, JSON
+
+  default_scope { where.not(status: 'failed') } unless ENV['CURRENT_PROJECT'] == 'kkcms'
+
+  enum body_type: { long: 'long', short: 'short' }
+  enum status: { pending: 'pending', passed: 'passed', failed: 'failed' }
+
+  def self.excellent
+    where(excellent: true)
+  end
+
+  def long?
+    body_type.eql? 'long'
+  end
+
+  def total_comments
+    comments_count + replies_count
+  end
+
+  def total_views
+    topic_counter.page_views + topic_counter.view_increment
+  end
+end
--- user.rb
+++ user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   include UserCreator
   mount_uploader :avatar, ImageUploader
 
+  has_many :topics, dependent: :destroy
+
   # 刷新访问时间
   def touch_visit!
     self.last_visit = Time.zone.now
@@ -13,5 +15,16 @@ class User < ApplicationRecord
 
   def avatar_path
     avatar.url.presence || wx_avatar
+  end
+
+  def silenced!(reason, till)
+    update(silenced: true,
+           silence_at: Time.zone.now,
+           silence_reason: reason,
+           silence_till: till)
+  end
+
+  def silenced_and_till?
+    silenced? && (silence_till.to_i > Time.zone.now.to_i)
   end
 end
